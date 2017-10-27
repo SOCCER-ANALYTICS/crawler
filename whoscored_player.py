d526909bafd25b0e5f41605a74c20c08282df3ad
lfsc09 <lfsc09@gmail.com>
2017-10-27 16:29:45 -0200

    Faltou um import

--- whoscored_player.py
+++ whoscored_player.py
@@ -1,7 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import NoSuchElementException     
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.by import By
 import time
 
