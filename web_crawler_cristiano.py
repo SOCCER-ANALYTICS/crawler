e9880562c15d01467dd815a673fab9a461a29a65
cristianofontana <fontana.c12@gmail.com>
2017-11-15 10:01:40 -0200

    Update web_crawler_cristiano.py

--- web_crawler_cristiano.py
+++ web_crawler_cristiano.py
@@ -1,17 +1,17 @@
-#Data: 20171107
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
-#Sofa score
 driver = webdriver.Firefox()
 
-driver.get("https://www.sofascore.com/pt/abc-fc-gremio-esportivo-brasil/LOsNLi")
-driver.find_element_by_xpath("""/html/body/div[4]/div/div[2]/div/div[2]/ul/li[2]/a""").click()
-
+driver.get("https://www.sofascore.com/pt/londrina-guarani/xOsxP")
+  
+driver.find_element_by_xpath("/html/body/div[4]/div/div[2]/div/div[2]/ul/li[2]/a").click()
 #jogador - chutes - desarmes - totpasses- duelos vencidos - min jogados - posicao - avaliacao
 
 x = 1 
 while x <= 22:  
-    varXpath = "/html/body/div[4]/div/div[2]/div/div[2]/div[2]/div[2]/div/div/div[2]/div[1]/table/tbody/tr["+str(x)+"]"
+    varXpath = """//*[@id="player-statistics-tab-summary"]/table/tbody/tr["""+str(x)+"""]"""
     posts = driver.find_elements_by_xpath(varXpath)
     for post in posts:
         print(post.text)
-    x = x + 1   
+    x = x + 1  
